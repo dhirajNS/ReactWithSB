1db0a235cc0e56957b4492366d6842b13c0af14a
dhirajNS <dhiraj.shetty@neosoftmail.com>
2023-10-27 19:30:19 +0530

    redux configuration

---
+++ src/reducers/IncDecrReducer.js
@@ -0,0 +1,12 @@
+import { act } from "react-dom/test-utils";
+
+const inititalState=0;
+const changeTheNumber=(state=inititalState,action)=>{
+    switch (action.type){
+        case "INCREMENT":return state+1;
+        case "DECREMENT":return state-1;
+        default: return state;
+    }
+}
+
+export default changeTheNumber;
---
+++ src/reducers/ReducerIndex.js
@@ -0,0 +1,7 @@
+import changeTheNumber from "./IncDecrReducer";
+
+import { combineReducers} from "redux";
+
+const rootReducers = combineReducers({changeTheNumber});
+
+export default rootReducers;
